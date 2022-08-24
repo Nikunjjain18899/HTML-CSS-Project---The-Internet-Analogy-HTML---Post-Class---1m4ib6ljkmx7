a84b279b2f716c57f4223eb0c6d6492bd0c732f1
Nikunjjain18899 <nikunjjain18899@gmail.com>
2022-08-25 00:20:52 +0530

    Mobile Tab Navigation

--- App.js
+++ App.js
@@ -1,1 +1,20 @@
+const contents= document.querySelectorAll('.content');
+const listitem= document.querySelectorAll('nav ul li');
 
+listitem.forEach((item, idx)=>{
+    item.addEventListener('click' ,()=>{
+        hideAllContents();
+        hideAllItems();
+
+        item.classList.add('active');
+        contents[idx].classList.add('show');
+    });
+});
+
+function hideAllContents(){
+    contents.forEach(contents=> contents.classList.remove('show'))
+};
+
+function hideAllItems(){
+    contents.forEach(item=> item.classList.remove('active'))
+};
